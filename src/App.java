c113dea752e0c8384b27bb99ee60389c36cd613f
ritakool <jokernguyen1994@gmail.com>
2023-03-21 23:25:42 +0700

    done

---
+++ src/App.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Date now = new Date();
+        System.out.println("Date now: " + now);
+    }
+}
